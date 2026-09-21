a8cc689d9978922f06b2e1582aa34ade9c18c074
Jav991 <jgomezcomino04@gmail.com>
2026-09-21 08:15:08 +0200

    Bloque 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿/*Console.WriteLine("Hello, World!");*/
+
+/* Ejercicio 1 Primera función 😎*/
+class Program
+{
+   static void Main(string[] args)
+    {
+        mostrarMensaje();
+    }
+    static void mostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al programa");
+    }
+}
