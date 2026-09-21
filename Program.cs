942e28c72ef843cf9b9928c6ca6c4e426b06bb1b
Carolinacacia07 <anacarolinaacaciosoares170596@gmail.com>
2026-09-21 08:47:50 +0200

    Bloque 1 😴

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Console.WriteLine("Hello, World!");*/
 
 /* Ejercicio 1 Primera función 😎*/
-class Program
+/*class Program
 {
    static void Main(string[] args)
     {
@@ -10,5 +10,23 @@ class Program
     static void mostrarMensaje()
     {
         Console.WriteLine("Bienvenido al programa");
+    }
+}*/
+
+/*Ejercicio 2*/
+
+class Parametro
+{
+    static void Main(string[] args)
+    {
+        String nombre = "";
+        Saludar(nombre);
+    }
+
+    static void Saludar(string nombre)
+    {
+        Console.WriteLine("Introduzca su nombre");
+        String n = Console.ReadLine()!;
+        Console.WriteLine($"Hola tu nombre es {n}");
     }
 }
